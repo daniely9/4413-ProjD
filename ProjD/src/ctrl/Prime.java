c6d2ac87d32d38a679b17de2ae637f34929f0bd5
daniely9 <danielyakubov92@gmail.com>
2018-10-29 12:37:51 -0400

    Add files via upload
    Prime Method Finished

--- ProjD/src/ctrl/Prime.java
+++ ProjD/src/ctrl/Prime.java
@@ -7,32 +7,61 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Prime
- */
-@WebServlet("/Prime")
+import model.Engine;
+
+
+@WebServlet("/Prime.do")
 public class Prime extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Prime() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+ 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		if (request.getParameter("calc") != null)
+		{
+			Engine engine = Engine.getInstance();
+			String min = request.getParameter("min");
+			String max = request.getParameter("max");
+			request.setAttribute("minRes", min);
+			request.setAttribute("maxRes", max);
+			try
+			{
+				request.setAttribute("result",  engine.doPrime(min, max));
+				System.out.println(request.getAttribute("result"));
+			} 
+			catch (Exception e)
+			{
+				request.setAttribute("error", e.getMessage());
+			}
+		} 
+		else if (request.getParameter("recalc") != null)
+		{	
+			Engine engine = Engine.getInstance();
+			String min = request.getParameter("min");
+			String max = request.getParameter("max");
+			String result ="";
+			try {
+				result = engine.doPrime(min, max);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			request.setAttribute("minRes", result);
+			request.setAttribute("maxRes", max);
+			
+			try
+			{
+				request.setAttribute("result",  engine.doPrime(result, max));
+				System.out.println(request.getAttribute("result"));
+			} 
+			catch (Exception e)
+			{
+				request.setAttribute("error", e.getMessage());
+			}
+		}
+		this.getServletContext().getRequestDispatcher("/Prime.jspx").forward(request, response);
+		
+		
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
--- ProjD/src/model/Engine.java
+++ ProjD/src/model/Engine.java
@@ -1,5 +1,45 @@
 package model;
 
+import java.math.BigInteger;
+import java.util.Random;
+
+import projA.*;
+
 public class Engine {
+	private static Engine instance = null;
+	public static final double BITS_PER_DIGIT = 3.0;
+	public static final Random RNG = new Random();
+
+	private Engine() {
+	}
+
+	public synchronized static Engine getInstance() {
+		if (instance == null)
+			instance = new Engine();
+		return instance;
+	}
+
+	public String doPrime(String lower, String upper) throws Exception {
+
+		try {
+			int low = Integer.parseInt(lower);
+			int high = Integer.parseInt(upper);
+			System.out.println(low + " " + high);
+			if (low < 0) {
+				throw new Exception("start < 0:" + low);
+			} else if (low >= high) {
+				throw new Exception("No more primes in range.");
+			} else {
+				BigInteger result = BigInteger.valueOf((long) low).nextProbablePrime();
+				if (result.intValue() >= high) {
+					throw new Exception("No more primes in range.");
+				} else {
+					return (result.toString());
+				}
+			}
+		} catch (Exception e) {
+			throw new Exception("The entry must be int!");
+		}
+	}
 
 }
